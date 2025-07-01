df5d50b01964dc5b6449d9978aab10a499059a92
叶启升 <2482314394@qq.com>
2025-07-01 23:56:28 +0800

    template start

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
+console.log(App);
 
 const app = createApp(App)
-app.mount(document.querySelector("#app"))
+console.log(app);
+// 使用 document.getElementById 确保能正确获取挂载点并挂载应用
+app.mount(document.getElementById("app"))
+
